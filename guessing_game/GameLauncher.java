a53c7d8baa8cce63694c62ce5c303877f773922f
pawelh12010279 <70893832+pawelh12010279@users.noreply.github.com>
2020-09-16 11:18:47 +0100

    Add files via upload

---
+++ guessing_game/GameLauncher.java
@@ -0,0 +1,9 @@
+package guessing_game;
+
+public class GameLauncher {
+    public static void main(String[] args) {
+        GuessingGame game = new GuessingGame();
+        game.startGame();
+    }
+
+} // end class
---
+++ guessing_game/Player.java
@@ -0,0 +1,12 @@
+package guessing_game;
+
+public class Player {
+
+    int number = 0; // where the guess goes
+
+    public void guess() {
+        number = (int) (Math.random() * 10);
+        System.out.println("I’m guessing "+ number);
+    }
+
+} // end class
